7164b81be65ca0cf60bc82114ed1f333079aeabf
EdiscoKes <eskoranteng003@st.ug.edu.gh>
2026-03-18 03:06:19 +0000

    fixed error issues

--- apis/paymentApi.js
+++ apis/paymentApi.js
@@ -1,3 +1,3 @@
 import API from "./apiClient";
 export const initializePayment =()=>API.post('/api/initialize/payment')
-export const verifyPayment =(data)=>API.post('/api/verify/payment',data)
+export const verifyPayment =(reference,data)=>API.post(`/api/verify/payment/${reference}`,data)
